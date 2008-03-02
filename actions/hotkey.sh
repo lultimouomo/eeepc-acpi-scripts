0389bdfb93ccbdf4694afe8df016a09e5df38de5
Nico Golde <nion@debian.org>
2008-03-02 17:26:46 +0100

    add kill before aosd_cat call and fix changelog, thanks ecc

--- actions/hotkey.sh
+++ actions/hotkey.sh
@@ -14,6 +14,7 @@ notify() {
 	user=$(who | sed -n '/ (:0[\.0].*)$\| :0 /{s/ .*//p;q}')
 	XAUTHORITY=/home/$user/.Xauthority
 	[ -f $XAUTHORITY ] && export XAUTHORITY
+	killall -q aosd_cat
 	if [ -n "$2" -a -z "$(echo $2 | sed 's/[0-9]//g')" ]; then
 		echo "$@%" | aosd_cat -f 0 -u 100 -o 0 -n "$OSD_FONT" &
 	else

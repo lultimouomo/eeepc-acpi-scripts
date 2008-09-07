dc50eb33e2757c7f5fa8b18de0b74d011b772a54
Damyan Ivanov <dmn@debian.org>
2008-09-07 12:15:14 +0300

    wireless: load/unload the wireless kernel module only for madwifi
    ralink does not need this, hot-plugging works for it allright
    
    ath5k not tested

--- actions/wireless.sh
+++ actions/wireless.sh
@@ -10,18 +10,23 @@ case $1 in
 	if [ $(cat $wlan_control) = 0 ]; then
 	    echo 1 > $wlan_control
             detect_wlan
-	    modprobe $WLAN_MOD
-	    # adding a sleep here, due to some bug the driver loading is not atomic here
-	    # and could cause ifconfig to fail
-	    sleep 1
-	    if ! ifconfig $WLAN_IF up; then exec $0 off; fi
+	    if [ "$WLAN_MOD" = 'ath_pci' ]; then
+		# madwifi needs some handholding
+		modprobe $WLAN_MOD
+		# adding a sleep here, due to some bug the driver loading is not atomic here
+		# and could cause ifconfig to fail
+		sleep 1
+		if ! ifconfig $WLAN_IF up; then exec $0 off; fi
+	    fi
 	fi
 	;;
     off|disable)
 	if [ $(cat $wlan_control) = 1 ]; then
             detect_wlan
-	    ifdown --force $WLAN_IF
-	    modprobe -r $WLAN_MOD
+	    if [ "$WLAN_MOD" = 'ath_pci' ]; then
+		ifdown --force $WLAN_IF
+		modprobe -r $WLAN_MOD
+	    fi
 	    echo 0 > $wlan_control
 	fi
 	;;

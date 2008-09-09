ea44294d7d8c2dbb49a2951157e0dc1f363b42cf
Damyan Ivanov <dmn@debian.org>
2008-09-09 08:36:52 +0300

    move notify() to lib/notify.sh
    notify.sh now can be sourced by other scripts that need user
    notification

--- actions/hotkey.sh
+++ actions/hotkey.sh
@@ -5,52 +5,8 @@
 
 . /etc/default/eeepc-acpi-scripts
 . /usr/share/eeepc-acpi-scripts/functions.sh
+. /etc/acpi/lib/notify.sh
 code=$3
-
-notify() {
-    CATEGORY=$1
-    MSG=$2
-    if [ -n "$3" ]; then
-	echo "usage: notify 'catgory' 'message text'" > /dev/stderr
-	return 1
-    fi
-    echo "$MSG"  # for /var/log/acpid
-    if [ -S /tmp/.X11-unix/X0 ]; then
-        detect_x_display
-
-    if [ "x$ENABLE_OSD" = "xno" ]; then
-        return
-    fi
-
-    OSD_SHOWN=
-
-    # try to show a nice OSD notification via GNOME OSD service
-    GOSDC=/usr/bin/gnome-osd-client
-    if [ -x $GOSDC ]; then
-	if ps -u $user -o cmd= | grep -q '^/usr/bin/python /usr/bin/gnome-osd-event-bridge'; then
-	    if echo "$MSG" | grep -q '[0-9]'; then
-		animations='off'
-	    else
-		animations='on'
-	    fi
-	    echo "<message id='eee-$CATEGORY' osd_fake_translucent_bg='off' osd_vposition='bottom' animations='$animations' hide_timeout='1200' osd_halignment='center'>$MSG</message>" \
-		| sudo -u $user $GOSDC -s --dbus
-	    OSD_SHOWN=1
-	fi
-    fi
-
-    if [ -z "$OSD_SHOWN" ]; then
-	killall -q aosd_cat
-	if [ -n "$MSG" -a -z "$(echo $MSG | sed 's/.*[0-9]\+//g')" ]; then
-		echo "$MSG%" | aosd_cat -f 0 -u 100 -o 0 -n "$OSD_FONT" &
-	else
-		echo "$MSG" | aosd_cat -n "$OSD_FONT" -f 100 -u 1000 -o 100 &
-	fi
-    fi
-    else
-	echo "$MSG" > /dev/console
-    fi
-}
 
 show_wireless() {
     detect_wlan

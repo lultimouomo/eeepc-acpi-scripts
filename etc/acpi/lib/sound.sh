4b5e792f15b562d3f6317f96f5a5cd21fe28325d
Darren Salt <linux@youmustbejoking.demon.co.uk>
2009-02-07 14:59:50 +0000

    Add audio control blacklisting.
    The first blacklisted string is "Mic", needed for ALC662.

--- etc/acpi/lib/sound.sh
+++ etc/acpi/lib/sound.sh
@@ -1,9 +1,14 @@
+# Parse amixer output for a regexp (first parameter).
+# Some controls are blacklisted; for this, we use a set of fixed strings in
+# /etc/acpi/lib/eeepc-amixer-blacklist. (No wildcards or regexps.)
 configureSoundFilter() {
   $AMIXER |
   grep -B1 "$1" |
-  sed -r "s/^(.*'([^']+)'.*|[^']+())$/\\2/; /^$/ d"
+  sed -r "s/^(.*'([^']+)'.*|[^']+())$/\\2/; /^$/ d" |
+  grep -ivFf /etc/acpi/lib/eeepc-amixer-blacklist
 }
 
+# Defaults
 configureSound() {
 
     [ "$SOUND_LABEL" ] || {

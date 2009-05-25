3a685c737cae5e94d6553d9d12788b1c6583d306
Raphael Geissert <atomo64@gmail.com>
2009-05-24 21:06:23 -0500

    Allow fine-grained control over the lid-closing action.
    Introduce LID_CLOSE_ACTION to allow users to control whether the laptop
    should be suspended, a specified command executed, or nothing be done
    when the lid is closed.

--- etc/acpi/actions/lid.sh
+++ etc/acpi/actions/lid.sh
@@ -4,6 +4,7 @@
 
 . /usr/share/acpi-support/policy-funcs
 
+
 # If powersaved is running, let it process the acpi event
 if pidof powersaved; then
 	exit 0
@@ -13,6 +14,18 @@ if [ `CheckPolicy` = 0 ] ; then
 	exit 0
 fi
 
-[ -e /etc/acpi/actions/suspend.sh ] || exit 0
+[ -e /etc/default/eeepc-acpi-scripts ] || exit 0
+. /etc/default/eeepc-acpi-scripts
 
-. /etc/acpi/actions/suspend.sh
+case "$LID_CLOSE_ACTION" in
+    nothing)
+	exit 0
+    ;;
+    suspend|"")
+	[ -e /etc/acpi/actions/suspend.sh ] || exit 0
+	. /etc/acpi/actions/suspend.sh
+    ;;
+    *)
+	$LID_CLOSE_ACTION
+    ;;
+esac

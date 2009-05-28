01c7db173912d73b8ef68ecb35e060adadbb5814
Darren Salt <linux@youmustbejoking.demon.co.uk>
2009-05-28 01:31:29 +0100

    Get the number of S.H.E. states from b8-b15 of the reported value. This fixes things on Celeron-based EeePCs' S.H.E.s, which only have two power control states, unlike the Atom-based ones, which have three.

--- etc/acpi/lib/shengine.sh
+++ etc/acpi/lib/shengine.sh
@@ -3,6 +3,8 @@
 # to be sourced
 
 SHENGINE_CTL=/sys/devices/platform/eeepc/cpufv
+SHENGINE_LIMIT="$(cat "$SHENGINE_CTL" 2>/dev/null)"
+SHENGINE_LIMIT=$(( ${SHENGINE_LIMIT:-768} >> 8 ))
 
 get_shengine()
 {
@@ -16,7 +18,7 @@ cycle_shengine()
 {
     if [ -e "$SHENGINE_CTL" ]; then
 	get_shengine
-	echo $(( ($? + 1) % 3)) > "$SHENGINE_CTL"
+	echo $(( ($? + 1) % $SHENGINE_LIMIT)) > "$SHENGINE_CTL"
     fi
     get_shengine
 }

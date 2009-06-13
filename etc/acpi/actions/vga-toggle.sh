17725af45e40cf5c1c836c0c76eb540a2e83b3c1
Santi Béjar <santi@agolina.net>
2009-06-14 00:20:11 +0200

    When KMS (kernel mode setting) is used the output names have a number in the end (VGA1 and LVDS1).

--- etc/acpi/actions/vga-toggle.sh
+++ etc/acpi/actions/vga-toggle.sh
@@ -10,8 +10,14 @@ if [ -e "$DEFAULT" ]; then . "$DEFAULT"; fi
 . $FUNC_LIB
 
 # return: 0 on disconnect, 1 on connected vga, 2 else
+# set VGA (and LVDS) to the output name, VGA or VGA1 (LVDS or LVDS1)
 getvga_status(){
-    STATUS=$( xrandr -q | grep VGA | cut -d ' ' -f 2,3 )
+    STATUSLINE=$( xrandr -q | grep VGA)
+    STATUS=$( echo $STATUSLINE | cut -d ' ' -f 2,3 )
+    VGA=$( echo $STATUSLINE | cut -d ' ' -f 1 )
+    # To not call xrandr again
+    NUM=${VGA##VGA}
+    LVDS=LVDS$NUM
     case "$STATUS" in
     disconnected*)
         return 0
@@ -30,9 +36,9 @@ getvga_status;
 # handle return value
 case $? in
     1)
-        xrandr --output VGA ${VGA_ON:---auto} --output LVDS ${LVDS_OFF:---off}
+        xrandr --output $VGA ${VGA_ON:---auto} --output $LVDS ${LVDS_OFF:---off}
         ;;
     *)
-        xrandr --output VGA --off --output LVDS --auto
+        xrandr --output $VGA --off --output $LVDS --auto
 esac
 
